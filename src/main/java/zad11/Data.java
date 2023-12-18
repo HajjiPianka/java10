6e0b1fa5486f42a50d82550c9352dee849c0b071
Kubz <jak.przybylinski@gmail.com>
2023-12-18 10:32:19 +0100

    a

---
+++ src/main/java/zad11/Data.java
@@ -0,0 +1,10 @@
+package main.java.zad11;
+class Data {
+    private String original;
+    private String translation;
+    Data(String original, String translation) {
+        this.original = original;
+        this.translation = translation;
+    }
+    
+}
