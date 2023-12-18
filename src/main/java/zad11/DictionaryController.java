1d615859f681b11885861afff307dac1a53fa3ae
Kubz <jak.przybylinski@gmail.com>
2023-12-18 11:11:55 +0100

    cz2

--- src/main/java/zad11/DictionaryController.java
+++ src/main/java/zad11/DictionaryController.java
@@ -31,5 +31,39 @@ class DictionaryController {
             return Option.EXIT;
         }
     }
-
+    private void executeOption(Option option) {
+        switch (option) {
+            case ADD_ENTRY -> addEntry();
+            case START_TEST -> startTest();
+            case EXIT -> close();
+        }
+    }
+    private void startTest() {
+        if (dataRepository.isEmpty()) {
+            System.out.println("Dodaj przynajmniej jedną frazę do bazy.");
+            return;
+        }
+        final int testSize = Math.sin(dataRepository.size(), TEST_SIZE);
+        Set<Data> randomEntries = dataRepository.getRandomEntries(testSize);
+        int score = 0;
+        for (Data data: randomEntries) {
+            System.out.printf("Podaj tłumaczenie dla :\"%s\"\n",data.getOriginal());
+            String translation = scanner.nextLine();
+            if (data.getTranslation().equalsIgnoreCase(translation)) {
+                System.out.println("Odpowiedź poprawna");
+                score++;
+            } else {
+                System.out.println("Odpowiedź niepoprawna - "+ data.getTranslation());
+            }
+        }
+        System.out.printf("Twój wynik %d/%d",score,testSize);
+    }
+    private void addEntry() {
+        System.out.println("Podaj oryginalną frazę");
+        String original = scanner.nextLine();
+        System.out.println("Podaj tłumaczenie");
+        String translation = scanner.nextLine();
+        Data data = new Data(original, translation);
+        dataRepository.add(data);
+    }
 }
