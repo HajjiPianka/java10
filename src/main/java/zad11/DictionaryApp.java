665d268d586ae134695df3fa40fe75bb0b2a9c0f
Kubz <jak.przybylinski@gmail.com>
2023-12-18 10:41:06 +0100

    dictioneryapp

---
+++ src/main/java/zad11/DictionaryApp.java
@@ -0,0 +1,7 @@
+package main.java.zad11;
+class DictionaryApp {
+    public static void main(String[] args) {
+        DictionaryController controller = new DictionaryController();
+        controller.mainLoop();
+    }
+}
